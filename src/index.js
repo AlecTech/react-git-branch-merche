8fa776a41425f977daedd8daf0e4b8483f2b1f45
AlecTech <eremeevoleg@gmail.com>
2020-09-17 11:11:32 -0600

    Added new component HelloStudent

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import HelloStudent from './components/HelloStudent';
 
 
 ReactDOM.render(
   <React.StrictMode>
-   <></>
+   <h1> My Git Test</h1>
+   <HelloStudent name="Eboka"/>
+   <HelloStudent name="Bob"/>
   </React.StrictMode>,
   document.getElementById('root')
 );
@@ -13,4 +16,4 @@ ReactDOM.render(
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+
